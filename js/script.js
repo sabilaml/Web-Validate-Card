40321acc825353994f62f8784537327bed36af8b
sabilastorage <sabilastorage@gmail.com>
2024-03-29 01:49:19 +0700

    Update Website content

--- js/script.js
+++ js/script.js
@@ -1,24 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector("#card-form");
     form.addEventListener("submit", function(event) {
-        event.preventDefault(); // Mencegah pengiriman formulir default
+        event.preventDefault();
         const cardNumber = document.querySelector('#credit_card_number').value;
         
-        // Membuat permintaan POST menggunakan Fetch API
-        fetch('validate.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: `credit_card_number=${encodeURIComponent(cardNumber)}`
-        })
-        .then(response => response.text())
-        .then(result => {
-            // Menampilkan hasil validasi di dalam elemen dengan id 'validation-result'
-            document.getElementById('validation-result').innerHTML = result;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+        function isValidCardNumber(number) {
+            const regex = new RegExp("^[0-9]{16}$");
+            if (!regex.test(number)) {
+                return false;
+            }
+            return number.split('').reverse().map(x => parseInt(x, 10))
+                .map((x, idx) => idx % 2 ? ((x * 2 > 9) ? (x * 2 - 9) : (x * 2)) : x)
+                .reduce((accum, x) => accum + x) % 10 === 0;
+        }
+
+        const validationResult = isValidCardNumber(cardNumber) ? 'Nomor kartu valid!' : 'Nomor kartu tidak valid!';
+        document.getElementById('validation-result').innerText = validationResult;
     });
 });
